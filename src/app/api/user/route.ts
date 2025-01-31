b4e83a1f14e0f86bd2d98a016954a496b44d29c7
Luiz Antônio Neto <luizneto.ifnmg@gmail.com>
2025-01-31 09:23:51 -0300

    feat: create user api

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,7 @@
 import { db } from "@/app/lib/prisma";
+import { userSchema } from "./validations";
+import { cognitoService } from "@/app/lib/aws";
+import { CognitoIdentityServiceProvider } from "aws-sdk";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
@@ -25,6 +28,126 @@ export async function GET(request: Request) {
 
     return new Response(
       JSON.stringify({ data: users, error: null, success: true }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: (error as Error).message,
+        success: false,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const validation = userSchema.safeParse(body);
+
+  if (!validation.success) {
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: validation.error.errors[0].message,
+        success: false,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const { aboutMe, address, birthDate, email, password } = validation.data;
+
+    const user = await db.$transaction(async (trx) => {
+      try {
+        const params: CognitoIdentityServiceProvider.AdminCreateUserRequest = {
+          MessageAction: "SUPPRESS",
+          TemporaryPassword: password,
+          UserAttributes: [
+            {
+              Name: "email",
+              Value: email,
+            },
+            {
+              Name: "custom:aboutMe",
+              Value: aboutMe || "Nothing here yet...",
+            },
+          ],
+          Username: email,
+          UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID || "",
+        };
+
+        const cognitoUser = await cognitoService
+          .adminCreateUser(params)
+          .promise();
+
+        if (!cognitoUser.User) {
+          throw new Error("User was not created correctly in Cognito!");
+        }
+
+        const subAttribute = cognitoUser.User?.Attributes?.find((attribute) => {
+          return attribute.Name === "sub";
+        });
+
+        if (!subAttribute) {
+          throw new Error("It was not possible to get the Cognito id!");
+        }
+
+        const cognitoId = String(subAttribute.Value);
+
+        const passwordUpdatePromise = cognitoService
+          .adminSetUserPassword({
+            Password: password,
+            Permanent: true,
+            Username: email,
+            UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID || "",
+          })
+          .promise();
+
+        const userCreationPromise = trx.user.create({
+          data: {
+            aboutMe,
+            ...(address && { address: { create: address } }),
+            birthDate,
+            cognitoId,
+            email,
+          },
+        });
+
+        const [_, user] = await Promise.all([
+          passwordUpdatePromise,
+          userCreationPromise,
+        ]);
+
+        return user;
+      } catch (error) {
+        try {
+          await cognitoService
+            .adminDeleteUser({
+              Username: email,
+              UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID || "",
+            })
+            .promise();
+        } catch (error) {}
+
+        throw error;
+      }
+    });
+
+    return new Response(
+      JSON.stringify({ data: user, error: null, success: true }),
       {
         headers: { "Content-Type": "application/json" },
         status: 200,
---
+++ src/app/api/user/validations.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+export const userAddressSchema = z.object({
+  streetAddress: z
+    .string({ required_error: "Street Address is required." })
+    .nonempty("Street Address is required."),
+  city: z
+    .string({ required_error: "City is required." })
+    .nonempty("City is required."),
+  state: z
+    .string({ required_error: "State is required." })
+    .nonempty("State is required."),
+  zipCode: z
+    .string({ required_error: "ZIP Code is required." })
+    .nonempty("ZIP Code is required."),
+});
+
+export const userSchema = z.object({
+  email: z
+    .string({ required_error: "Field password is required." })
+    .email()
+    .nonempty("Field email is required."),
+  password: z
+    .string({ required_error: "Field password is required." })
+    .min(8, "Password must contain at lest 8 characters.")
+    .max(16, "Password must contain up to 16 characters.")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter.")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter.")
+    .regex(/[\W_]/, "Password must contain at least one special character.")
+    .nonempty("Field password is required."),
+
+  aboutMe: z
+    .string()
+    .max(500, "About Me must contain up to 500 characters.")
+    .optional(),
+  birthDate: z.string().optional(),
+
+  address: userAddressSchema.optional(),
+});
